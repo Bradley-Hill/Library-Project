5bafbe94b091f25b3cdadab2f80e68c08c1bbab9
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-04-21 17:10:32 +0200

    constructor and addBook function added

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let grandLibrary = []
 
-function Book(author,title,pages,publishDate,){
-    this.author = author
-    this.title = title
-    this.pages = pages
-    this.publishDate = publishDate
+function Book(author,title,pages,publishDate,status){
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.publishDate = publishDate;
+    this.status = status;
+}
+
+function addBookToLibrary(author,title,pages,publishDate,status){
+    let book = Object.create(Book.prototype);
+    Book.call(book,author,title,pages,publishDate,status);
+    grandLibrary.push(book);
 }
