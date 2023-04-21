07c82fa4de08aa13f9fade5461964859930801b2
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-04-21 16:48:11 +0200

    Added array for library and constructor for Books

---
+++ script.js
@@ -0,0 +1,8 @@
+let grandLibrary = []
+
+function Book(author,title,pages,publishDate,){
+    this.author = author
+    this.title = title
+    this.pages = pages
+    this.publishDate = publishDate
+}
