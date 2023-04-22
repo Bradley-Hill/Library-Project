f95eecf7a875c42b5f3ad616f7acaa1c08a5904f
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-04-22 15:28:43 +0200

    added functionality to the addBookbutton+btn place

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
 let grandLibrary = [{
     author: 'Joe Abercrombie',
-    title:'The Heroes',
-    pages: 289,
-    publishDate: 2011, 
-    haveRead:true}]
+    title: 'The Heroes',
+    pages: 389,
+    publishDate: 2011,
+    haveRead: true
+},
+{
+    author: 'Terry Pratchett',
+    title: 'Thud!',
+    pages: 268,
+    publishDate: 2005,
+    haveRead: false
+}]
 
-function Book(author,title,pages,publishDate,haveRead){
+const gridContainer = document.getElementById('grid-container')
+const addBookButton = document.getElementById('add-entry');
+
+//for loop populating the grid with books
+function updateLibraryDisplay() {
+    gridContainer.innerHTML = '';
+    for (let i = 0; i < grandLibrary.length; i++) {
+        const book = grandLibrary[i];
+        const card = document.createElement('div');
+        card.classList.add('library-card');
+
+        const title = document.createElement('h2');
+        title.textContent = book.title;
+
+        const author = document.createElement('p');
+        author.textContent = book.author;
+
+        const pages = document.createElement('p');
+        pages.textContent = `${book.pages} pages`;
+
+        const publishDate = document.createElement('p');
+        publishDate.textContent = `Published in ${book.publishDate}`;
+
+        const haveRead = document.createElement('p');
+        haveRead.textContent = book.haveRead ? 'Read' : 'Not read';
+
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(publishDate);
+        card.appendChild(haveRead);
+
+        gridContainer.appendChild(card);
+    }
+}
+
+//Constructor Function
+function Book(author, title, pages, publishDate, haveRead) {
     this.author = author;
     this.title = title;
     this.pages = pages;
@@ -13,10 +58,16 @@ function Book(author,title,pages,publishDate,haveRead){
     this.haveRead = haveRead;
 }
 
-function addBookToLibrary(author,title,pages,publishDate,haveRead){
-    let book = Object.create(Book.prototype);
-    Book.call(book,author,title,pages,publishDate,haveRead);
-    grandLibrary.push(book);
+function addBookToLibrary(author, title, pages, publishDate, haveRead) {
+    grandLibrary.push(new Book(author, title, pages, publishDate, haveRead));
+    updateLibraryDisplay();
 }
 
+
+addBookButton.onclick = function () {
+    addBookToLibrary('Nicholas Eames', 'Kings of the Wyld', 422, 2017, true);
+    updateLibraryDisplay()
+}
+
+updateLibraryDisplay()
 console.log(grandLibrary)
