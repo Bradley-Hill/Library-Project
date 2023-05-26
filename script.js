9b00b4744e61d92dde50ee7120baf128c34bcc9e
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-05-26 11:27:51 +0200

    removed superfluous code

--- script.js
+++ script.js
@@ -19,61 +19,8 @@ const formContainer = document.getElementById('book-form');
 const submitButton = document.getElementById('submit-button');
 const cancelButton = document.getElementById('cancel-button');
 
-addBookButton.addEventListener('click',showForm);
+addBookButton.addEventListener('click',showForm());
 
-//for loop populating the grid with books in grandLibrary array
-// function updateLibraryDisplay() {
-//     gridContainer.innerHTML = '';
-//     for (let i = 0; i < grandLibrary.length; i++) {
-//         const book = grandLibrary[i];
-//         const card = document.createElement('div');
-//         card.classList.add('library-card');
-//         card.dataset.index = i;
-
-//         const title = document.createElement('h1');
-//         title.textContent = book.title;
-
-//         const author = document.createElement('h3');
-//         author.textContent = book.author;
-
-//         const pages = document.createElement('p');
-//         pages.textContent = `${book.pages} pages`;
-
-//         const publishDate = document.createElement('p');
-//         publishDate.textContent = `Published in ${book.publishDate}`;
-
-//         const haveRead = document.createElement('p');
-//         haveRead.textContent = book.haveRead ? 'Read' : 'Not read';
-
-//         const toggleReadBtn = document.createElement('button');
-//         toggleReadBtn.classList.add('switch');
-//         toggleReadBtn.textContent = 'Toggle Read';
-//         toggleReadBtn.addEventListener('click', () => {
-//         const index = card.dataset.index;
-//         toggleHaveRead(index);
-//     });
-
-//         const removeBtn = document.createElement('button');
-//         removeBtn.classList.add('remove');
-//         removeBtn.textContent = 'Remove?';
-//         removeBtn.addEventListener('click', () => {
-//             const index = card.dataset.index;
-//             grandLibrary.splice(index,1);
-//             updateLibraryDisplay();
-//         })
-
-
-//         card.appendChild(title);
-//         card.appendChild(author);
-//         card.appendChild(pages);
-//         card.appendChild(publishDate);
-//         card.appendChild(haveRead);
-//         card.appendChild(removeBtn);
-//         card.appendChild(toggleReadBtn);
-
-//         gridContainer.appendChild(card);
-//     }
-// }
 
 //Class for book objects.
 class Book {
@@ -153,26 +100,6 @@ class Book {
     }
 }
 
-
-//Constructor Function------------Unnecessary now.
-// function Book(author, title, pages, publishDate, haveRead) {
-//     this.author = author;
-//     this.title = title;
-//     this.pages = pages;
-//     this.publishDate = publishDate;
-//     this.haveRead = haveRead;
-// }
-
-// function addBookToLibrary(book) {
-//     if(grandLibrary.some(b=>b.author === book.author && b.title === book.title)){
-//         alert('Book already exists in library!');
-//         return;
-//     }
-//     grandLibrary.push(book);
-//     updateLibraryDisplay();
-//     hideForm();
-// }
-
 function submitForm(event) {
     event.preventDefault();
     const author = document.getElementById('author-input').value;
@@ -197,12 +124,6 @@ function showForm() {
 function hideForm() {
     formContainer.style.display = 'none'
 }
-
-// Moved to being a method of the class Book
-// function toggleHaveRead(index){
-//     grandLibrary[index].haveRead = !grandLibrary[index].haveRead;
-//     updateLibraryDisplay();
-// }
 
 addBookButton.onclick = function () {
     showForm();
